ffa5ca9a0b3a183a7f60021977f725c4062ae4b1
Robertm339 <71312299+Robertm339@users.noreply.github.com>
2022-06-18 14:52:43 -0700

    Initial Commit

---
+++ FlashZilla/FlashZillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashZillaApp.swift
+//  FlashZilla
+//
+//  Created by Robert Martinez on 6/18/22.
+//
+
+import SwiftUI
+
+@main
+struct FlashZillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
